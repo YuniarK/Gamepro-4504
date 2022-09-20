6b7fefeed4b1b731e3462935ff96f70a4af3d838
Yuniar Kurniawan <113452902+YuniarK@users.noreply.github.com>
2022-09-21 02:20:17 +0700

    Add files via upload

---
+++ Script Pertemuan2/CobaJalan.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CobaJalan : MonoBehaviour {
+    public float movespeed;
+    int status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.TransLate(movespeed, 0, 0); //bisa diganti kode ini
+        transform.position = new Vector3 (transform.position.x + movespeed, transform.position.y);
+    }
+}
